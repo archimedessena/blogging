8b66498eef07235d7b76252916fce107ad05006f
Archimedes <archimedessena@gmail.com>
2020-09-06 15:42:29 +0000

    home

---
+++ blogs/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_image = models.ImageField(upload_to='img', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+     
+     
+    def __str__(self):
+        return self.title
+    
+
+     #def get_absolute_url(self):
+     #   return reverse('post-detail', kwargs={'pk': self.pk})
+    
+        
